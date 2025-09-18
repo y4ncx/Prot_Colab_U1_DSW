01ed711fbe7b6851dc11be4ac78e695959fc2507
y4ncx <y4ncxs@gmail.com>
2025-09-18 14:04:12 -0500

    Se agrega el caso de uso ListarMascotas

---
+++ src/application/usecases/ListarMascotas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Src\application\usecases;
+
+use Src\Domain\Entities\Mascota;
+use Src\Domain\Repositories\MascotaRepository;
+
+class ListarMascotas{
+    private MascotaRepository $repositorio;
+
+    public function __Construct(MascotaRepository $repositorio){
+        $this->repositorio = $repositorio;
+    }
+
+    public function execute(Mascota $masccota): void{
+        $this->repositorio->crear($masccota);
+    }
+}
